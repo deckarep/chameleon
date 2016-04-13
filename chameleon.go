ce45becc8ec5923e298b74db5cd9f360a2883e51
logikal <sean.kilgore@sendgrid.com>
2016-04-13 13:21:45 -0700

    handle SIGUSR1 to toggle Healthy bool

--- chameleon.go
+++ chameleon.go
@@ -8,29 +8,33 @@ import (
 	"log"
 	"time"
   "strconv"
+	"os/signal"
+	"os"
+	"syscall"
 )
 
 /*
-Masquerade: Service name to masquerade as
-Port: The port to listen on
-Healthy: Default healthy or not
-MinWait: Minimum time to wait before responding
-MaxWait: Maximum time to wait before responding
-
-e.g.
 export CHAMELEON_MASQUERADE=apid
 export CHAMELEON_PORT=9999
 export CHAMELEON_HEALTHY=false
 export CHAMELEON_MINWAIT="100ms"
 export CHAMELEON_MAXWAIT="1s"
-
 */
 
 type Specification struct {
+  // Masquerade: Service name to masquerade as
 	Masquerade string
+
+  // Port: The port to listen on
 	Port       int
+
+  // Healthy: Default healthy or not
 	Healthy    bool
+
+  // MinWait: Minimum time to wait before responding
 	MinWait    time.Duration
+
+  // MaxWait: Maximum time to wait before responding
 	MaxWait    time.Duration
 }
 
@@ -51,12 +55,34 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	app := martini.Classic()
-	app.Use(render.Renderer())
-  portformat := ":%s"
+	// set up a channel to receive signals on.
+	sig := make(chan os.Signal, 1)
+  signal.Notify(sig, syscall.SIGUSR1)
 
-  app.RunOnAddr(fmt.Sprintf(portformat, strconv.Itoa(s.Port)))
-	// run application
-	app.Run()
+	// If we get SIGUSR1, toggle the Healthy state.
+	go func() {
+		for {
+			select{
+			case <-sig:
+				log.Println("Received USR1 signal")
+				s.Healthy = !s.Healthy
+				_, err = fmt.Printf(format, s.Masquerade, s.Port, s.Healthy, s.MinWait, s.MaxWait)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+			}
+		}
+	}()
+
+  // set up the webserver
+	chameleon := martini.Classic()
+	chameleon.Use(render.Renderer())
+
+  // start on the port we specify rather than the default
+  portformat := ":%s"
+  chameleon.RunOnAddr(fmt.Sprintf(portformat, strconv.Itoa(s.Port)))
+
+	// start it up!
+	chameleon.Run()
 
 }
